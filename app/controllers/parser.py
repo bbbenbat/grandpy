24ff96d26f40e4bf38b9cfce68e4ac7a05003bac
bbbenbat <bbessayah@gmail.com>
2020-12-23 18:17:10 +0100

    pytest delete_punctuation function

--- app/controllers/parser.py
+++ app/controllers/parser.py
@@ -1,18 +1,29 @@
 """
 
 """
+import string
+import re
 
 class Checkdata:
     """  """
     def __init__(self):
-        self.word
+        pass
 
 
-    def check_word(self, req):
+    def low_case(self, req):
+        """ Change all caracters in low-case. """
+        return req.lower()
+
+    def delete_punctuation(self, req):
+        """ Replace all punctuation by whitespace. """
+        return req
+
+
+def check_word(self, req):
         """
-        enlever les majuscules
         enlever les ponctuations
         mettre les mots dans une liste avec un split par les espaces
         application du filtre
         """
         return req
+
--- test/test_parser.py
+++ test/test_parser.py
@@ -9,13 +9,26 @@ from app.controllers import parser
 # Quand... j'accompli une action
 # Alors... je constate que
 
+pars = parser.Checkdata()
+QUESTION = "Salut GrandPy, comment vas-tu? " \
+               "Peux-tu me dire où se trouve l'Arc de Triomphe?"
 
-def test_check_data_question():
+def test_low_case():
     """  """
-    pars = parser.Checkdata()
-    question = "Salut GrandPy, comment vas-tu? " \
-               "Peux-tu me dire où se trouve l\'Arc de Triomphe?"
+    req = pars.low_case(QUESTION)
+    result = QUESTION.lower()
+    assert req == result
+
+def test_delete_punctuation():
+    """  """
+    req = pars.delete_punctuation(QUESTION)
+    result = "Salut GrandPy comment vas tu Peux tu me dire où se trouve l Arc de Triomphe "
+    assert req == result
+
+"""def test_check_data_question():
+    """  """
+
     result = "Arc de triomphe"
-    ret = pars.check_word(question)
-    assert result == ret
+    ret = pars.check_word(QUESTION)
+    result == ret"""
 
