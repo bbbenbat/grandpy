ebff1749bc3d0991333c3519bb320ef1bf35d0dd
bbbenbat <bbessayah@gmail.com>
2020-12-29 20:53:17 +0100

    test run_api (googlemaps)

--- app/models/google_api.py
+++ app/models/google_api.py
@@ -1,17 +1,21 @@
-import googlemaps
 import time
+
+import googlemaps
 
 
 class GoogleApi:
     """  """
 
-    def run(self, req):
+    def run_api(self, req):
         """  """
-        api_key = 'AIzaSyByb99q8Q6CPiWRlkKLnWD4SxwlBZ8o4co'
+        api_key = 'AIzaSyB6JPe27sHKX8AVcw-D3SZixIDi4ZcJHcM'
         gmaps_client = googlemaps.Client(api_key)
         geocode_result = gmaps_client.geocode(req, region='fr', language='fr')
+        return geocode_result
+
+    def run(self, req):
+        """  """
+        data = self.run_api(req)
         time.sleep(2)
-        result = geocode_result[0]
-        return result['formatted_address'], \
-               result['geometry']['location']['lat'], \
-               result['geometry']['location']['lng']
+        result = data[0]
+        return result['formatted_address'], result['geometry']['location']['lat'], result['geometry']['location']['lng']
---
+++ test/test_google_api.py
@@ -0,0 +1,36 @@
+from app.models import google_api
+
+google_map = google_api.GoogleApi()
+
+QUESTION = 'tour eiffel'
+GOOGLE_RES = [{'address_components': [{'long_name': '5', 'short_name': '5', 'types': ['street_number']},
+                                      {'long_name': 'Avenue Anatole France', 'short_name': 'Avenue Anatole France',
+                                       'types': ['route']},
+                                      {'long_name': 'Paris', 'short_name': 'Paris', 'types': ['locality', 'political']},
+                                      {'long_name': 'Département de Paris', 'short_name': 'Département de Paris',
+                                       'types': ['administrative_area_level_2', 'political']},
+                                      {'long_name': 'Île-de-France', 'short_name': 'IDF',
+                                       'types': ['administrative_area_level_1', 'political']},
+                                      {'long_name': 'France', 'short_name': 'FR', 'types': ['country', 'political']},
+                                      {'long_name': '75007', 'short_name': '75007', 'types': ['postal_code']}],
+               'formatted_address': 'Champ de Mars, 5 Avenue Anatole France, 75007 Paris, France',
+               'geometry': {'location': {'lat': 48.85837009999999, 'lng': 2.2944813},
+                            'location_type': 'ROOFTOP',
+                            'viewport': {'northeast': {'lat': 48.8597190802915, 'lng': 2.295830280291502},
+                                         'southwest': {'lat': 48.8570211197085, 'lng': 2.293132319708498}}
+                            },
+               'place_id': 'ChIJLU7jZClu5kcR4PcOOO6p3I0',
+               'plus_code': {'compound_code': 'V75V+8Q Paris, France', 'global_code': '8FW4V75V+8Q'},
+               'types': ['establishment', 'point_of_interest', 'tourist_attraction']
+               }]
+
+def test_googleApi_run_api():
+    """  """
+    req = google_map.run_api(QUESTION)
+    result = GOOGLE_RES
+    assert result == req
+
+
+
+
+
