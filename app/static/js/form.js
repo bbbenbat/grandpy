d5ee591a8836699d67dbe8a69b907a160d99c78d
bbbenbat <bbessayah@gmail.com>
2020-12-31 01:54:07 +0100

    update url tempo

--- app/static/js/form.js
+++ app/static/js/form.js
@@ -34,7 +34,7 @@ function go(){
 function search(value){
     jQuery.ajax({
           type:'GET',
-          url:'https://0.0.0.0:5000/tempo',
+          url:'/tempo',
           data:'question='+value,
           success:function(result){
             idmap = Math.random().toString(36).slice(-8);
